076823ac24fab886ecad9106843684110b6d1688
Thomas Mol <email@thomasmol.com>
2026-02-15 15:23:16 +0100

    Update notification.js

--- plugins/notification.js
+++ plugins/notification.js
@@ -2,10 +2,10 @@ export const NotificationPlugin = async ({ $ }) => {
   return {
     event: async ({ event }) => {
       if (event.type === "session.idle") {
-        await $`osascript -e 'display notification "Session completed" with title "opencode" sound name "Glass"'`;
+        await $`osascript -e 'display notification "Session completed" with title "OpenCode" sound name "Hero"'`;
       }
       if (event.type === "permission.asked") {
-        await $`osascript -e 'display notification "Permission requested" with title "opencode" sound name "Glass"'`;
+        await $`osascript -e 'display notification "Permission requested" with title "OpenCode" sound name "Morse"'`;
       }
     },
   };
